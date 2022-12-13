2c21352ccf8f655aff23c602aaf9fe07960e5dcd
Rob White <rob@tallguyrob.com>
2022-12-13 23:02:24 +0000

    Switch Elixir LS

--- .config/nvim/telescope.lua
+++ .config/nvim/telescope.lua
@@ -5,3 +5,16 @@ local utils = require('utils')
 telescope.setup()
 
 utils.map('n', '<leader>f', ':Telescope live_grep<CR>')
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "eex",
+    "elixir",
+    "erlang",
+    "heex",
+    "html",
+    "surface",
+  },
+  highlight = {enable = true},
+  disable = { "typescript" },
+})
